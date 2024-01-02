ccc39bc93ee59a8b15b8b3de6f6658256ad93690
wagnerlimanet <wagnerlima.net@gmail.com>
2024-01-02 18:09:28 -0300

    first commit

---
+++ produtos/camisas/linha-lendas/enviar-camisa-nascimento-das-lendas.php
@@ -0,0 +1,33 @@
+<?
+
+    # alterar a variavel abaixo colocando o seu email
+
+    $destinatario = "eu@wagnerlima.net";
+
+    $camisa = "Camisa Nascimento das Lendas";
+    $modelo = $_REQUEST['modelo'];
+    $tamanho = $_REQUEST['tamanho'];
+    $quantidade = $_REQUEST['quantidade'];
+    $email = "ideias@wagnerlima.net";
+    $mensagem = $_REQUEST['mensagem'];
+    $assunto = "KADOSH TSHIRTS";
+    
+     // monta o e-mail na variavel $body
+
+    $body = "Gostaria de encomendar " . $quantidade . " unidades da " . $camisa . ", modelo " . $modelo . " e tamanho " . $tamanho . "." . "\n";
+    $body = $body . "" . "\n";
+    $body = $body . "===================================" . "\n";
+    $body = $body . "Estampa: " . $camisa . ";" . "\n";
+    $body = $body . "Quantidade: " .$quantidade . " unidades;" . "\n";
+    $body = $body . "Modelo: " . $modelo . ";" . "\n";
+    $body = $body . "Tamanho: " .$tamanho . "." . "\n";
+    $body = $body . "===================================" . "\n";
+
+    // envia o email
+    mail($destinatario, $assunto , $body, "From: $camisa\r\n");
+    
+
+    // redireciona para a página de obrigado
+    header( "Location: https://pag.ae/7-7eaqv69" );
+
+    ?>
